d7d8da682fd270713f487f12bd7d8c432d209589
miguel medina <mike_wiii@hotmail.com>
2020-10-03 16:13:46 -0500

    Se agrego la plantilla

---
+++ OpenGLStart/Source/Main.cpp
@@ -0,0 +1,57 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Inicializamos la libreria */
+    if (!glfwInit())
+        return -1;
+
+
+    /* Cree una ventana en modo ventana y su contexto OpenGL */
+    window = glfwCreateWindow(1200, 720, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Actualice el contexto de la ventana */
+    glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK)
+        std::cout << "Error!" << std::endl;
+
+    /* Bucle hasta que el usuario cierre la ventana. */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render aqu� */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+            glVertex3f(-0.5f,0.5f,0);
+            glVertex3f(-0.5f,-0.5f,0);
+            glVertex3f(0.5f,-0.5f,0);
+			glColor3f(0.4f, 0.5f, 0.6f);
+        glEnd();
+
+		glBegin(GL_TRIANGLES);
+			glVertex3f(-0.5f, 0.5f, 0);
+			glVertex3f(0.5f, 0.5f, 0);
+			glVertex3f(0.5f, -0.5f, 0);
+			glColor3f(0.4f, 0.0f, 0.1f);
+		glEnd();
+   
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
