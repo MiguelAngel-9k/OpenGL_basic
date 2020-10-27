80579bde0bd90d36479046e1980fcff0cc6339cd
miguel medina <mike_wiii@hotmail.com>
2020-10-27 13:08:36 -0600

    no hay bugs

--- OpenGLStart/Source/Main.cpp
+++ OpenGLStart/Source/Main.cpp
@@ -6,22 +6,33 @@
 
 #include <iostream>
 
-//PROBANDO COMENTARIO
-//HOLA XD
+    GLFWwindow* window;
+
+#define CURSOR_CENTER(value) ((value/2))
 
 int main(void)
 {
-    GLFWwindow* window;
 
     /* Inicializamos la libreria */
-    if (!glfwInit())
-        return -1;
+	if (!glfwInit())
+	{
+		fprintf(stderr, "GLFW Failed");
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 
     /* Cree una ventana en modo ventana y su contexto OpenGL */
     window = glfwCreateWindow(1200, 720, "First window", NULL, NULL);
     if (!window)
-    {
+	{
+		fprintf(stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n");
+		getchar();
         glfwTerminate();
         return -1;
     }
@@ -29,8 +40,37 @@ int main(void)
     /* Actualice el contexto de la ventana */
     glfwMakeContextCurrent(window);
 
-    if (glewInit() != GLEW_OK)
-        std::cout << "Error!" << std::endl;
+	glewExperimental = true;
+	if (glewInit() != GLEW_OK)
+	{
+		fprintf(stderr, "Failed to initialize GLEW\n");
+		getchar();
+		glfwTerminate();
+		return -1;
+	}
+
+	//Tecla de ESC
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+	//Escondemos el mouse en la ventana
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+	//Colocamos el cursor en medio de la ventana
+	glfwPollEvents();
+	glfwSetCursorPos(window, CURSOR_CENTER(1200), CURSOR_CENTER(720));
+
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LESS);
+	glEnable(GL_CULL_FACE);
+
+	GLuint VertexArrayID;
+	glGenVertexArrays(1, &VertexArrayID);
+	glBindVertexArray(VertexArrayID);
+
+	//Creamos shaders compatibles
+	//GLuint programID = LoadShaders("TransformVertexShader.vertexshader", "TextureFragmentShader.fragmentshader");
+
+
+
 
     /* Bucle hasta que el usuario cierre la ventana. */
     while (!glfwWindowShouldClose(window))
